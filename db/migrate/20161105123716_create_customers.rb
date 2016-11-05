67d7aaca9ec3364a258b7cde943a660aed6c2e57
aswinsanakan <aswinsanakan@gmail.com>
2016-11-05 18:13:04 +0530

    Add customer model with name and address

---
+++ db/migrate/20161105123716_create_customers.rb
@@ -0,0 +1,10 @@
+class CreateCustomers < ActiveRecord::Migration
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :address
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105111145) do
+ActiveRecord::Schema.define(version: 20161105123716) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20161105111145) do
   end
 
   add_index "clients", ["deleted_at"], name: "index_clients_on_deleted_at"
+
+  create_table "customers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
