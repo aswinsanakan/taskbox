b7667adb504dfd992e13d087ba9ff1a03c679cbb
aswinsanakan <aswinsanakan@gmail.com>
2016-11-23 19:46:29 +0530

    pg installed

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
    :domain => "gmail.com",
    :authentication => "plain",
    :enable_starttls_auto => true,
-   :user_name => "aswinsanakan@gmail.com",
-   :password => "youngsherlock",
+   :user_name => "aswinunofficial@gmail.com",
+   :password => "unofficia1",
    :openssl_verify_mode  => 'none'
 }
